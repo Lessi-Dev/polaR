5d926fbf8afbb3a58161c7912eb336ad54270e6a
Lessi-Dev <Leo.boerl2006@gmail.com>
2022-03-23 22:49:57 +0100

    added basic file structure

--- src/app.js
+++ src/app.js
@@ -1,5 +1,28 @@
 const express = require('express');
 const fs = require('fs');
+const path = require('path');
 const mongoose = require('mongoose');
+const config = require('config.json');
 
 const app = express();
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname + '/../public/index.html'));
+})
+
+app.get('/Add', (req, res) => {
+    res.sendFile(path.join(__dirname + '/../public/add.html'));
+})
+
+app.get('/Search', (req, res) => {
+    res.sendFile(path.join(__dirname + '/../public/search.html'));
+})
+
+app.get('/img/:image', (req, res) => {
+    res.sendFile(path.join(__dirname + '/../public/img/' + req.params.image));
+
+})
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+})
